64cc579c73cac0962e051b32c0994612118cada4
Blari <Blarior@gmail.com>
2020-12-08 22:02:41 +0300

    Add content on main page Add demo

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,12 +1,58 @@
 <template>
-  <div class="div">
-    <h1>Hello World</h1>
-    <hr />
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="10" offset="1">
+        <template>
+          <v-expansion-panels popout>
+            <v-expansion-panel v-for="(test, i) in tests" :key="i">
+              <v-expansion-panel-header>
+                {{ test.title }}
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-card>
+                  <v-card-text>
+                    {{ test.description }}
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn color="primary">Start</v-btn>
+                    <v-btn color="primary">Example</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </template>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
+  data() {
+    return {
+      tests: [
+        {
+          id: 0,
+          title: "Карта интересов для младших школьников",
+          description:
+            "Для того, чтобы дать вам правильный совет и конкретные рекомендации для развития способностей " +
+            "вашего ребёнка, нам нужно знать его склонности. Вам предлагается 35 вопросов, подумайте и ответьте на " +
+            "каждый из них, стараясь не завышать и не занижать возможности ребёнка. Для большей  объективности сравните " +
+            "его с другими детьми того же возраста.",
+        },
+        {
+          id: 1,
+          title: "Методика «Тип мышления»",
+          description:
+            "Диагностика типа мышления респондента. Тип мышления – индивидуальный способ преобразования информации. " +
+            "Зная свой тип мышления, можно прогнозировать успешность в конкретных видах профессиональной деятельности.\n" +
+            "У каждого человека преобладает определенный тип мышления. Данный тест поможет вам определить тип своего мышления. " +
+            "Если вы согласны с высказыванием, то ставите “+”, если не согласны – “-“.",
+        },
+      ],
+    };
+  },
 };
 </script>
