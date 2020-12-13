b07f805c898aafee7e8e7e8be45e1558a4c61c78
Blari <Blarior@gmail.com>
2020-12-14 00:51:15 +0300

    Fix docs

--- src/Routes.js
+++ src/Routes.js
@@ -14,7 +14,7 @@ export default new Router({
   mode: "history",
   routes: [
     {
-      path: "/",
+      path: "/psy-tests/",
       name: "Layout",
       component: Layout,
       redirect: Dashboard,
